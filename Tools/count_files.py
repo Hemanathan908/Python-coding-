752e4546ce4751f9b6baa56fb0be3414a62fc059
E. Hemanathan <190844692+Hemanathan908@users.noreply.github.com>
2024-12-30 08:58:36 +0530

    Update count_files.py

--- Tools/count_files.py
+++ Tools/count_files.py
@@ -1,1 +1,20 @@
 
+# -*- coding: utf-8 -*-
+import os
+
+
+def count_files(path):
+    """Count number of files in a directory recursively.
+
+    Args:
+        path (str): Directory.
+
+    Returns:
+        int: Return number of files.
+
+    """
+    count = 0
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            count += 1
+    return count
